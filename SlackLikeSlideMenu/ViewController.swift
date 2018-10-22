d80e4b69503e00adfc560f431439adf42aa182bd
Nuno Pereira <nmfpereira@gmail.com>
2018-10-22 08:43:16 +0100

    Initial Commit

---
+++ SlackLikeSlideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlackLikeSlideMenu
+//
+//  Created by Nuno Pereira on 22/10/2018.
+//  Copyright © 2018 Nuno Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
