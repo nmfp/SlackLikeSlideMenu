61adf7ce318dbd19881e0d72f3de1b016bfe09ef
Nuno Pereira <nmfpereira@gmail.com>
2018-10-22 08:59:50 +0100

    Initial commit with a navigationController.

--- SlackLikeSlideMenu/AppDelegate.swift
+++ SlackLikeSlideMenu/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigationController
+        
         return true
     }
 
