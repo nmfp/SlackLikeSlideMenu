d11b83810a880d55b3359c8779c98ab6200583bd
Nuno Pereira <nmfpereira@gmail.com>
2018-10-22 09:10:28 +0100

    Setting up Initial TableViewController

--- SlackLikeSlideMenu/ViewController.swift
+++ SlackLikeSlideMenu/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupNavigationItems()
     }
 
+    func setupNavigationItems() {
+        navigationItem.title = "Home"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+    }
 
+    //MARK: Actions
+    @objc func handleOpen() {
+        print("opening...")
+    }
+    
+    @objc func handleHide() {
+        print("Hiding...")
+    }
+    
+    
+    //MARK: - TableViewController
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellId")
+        return cell
+    }
 }
 
