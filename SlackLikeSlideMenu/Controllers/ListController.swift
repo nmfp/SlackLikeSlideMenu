970c875ffd62fe2cfa36425f40e2b984f38ce810
Nuno Pereira <nmfpereira@gmail.com>
2018-11-01 22:29:54 +0000

    Showing different viewControllers according to menu item tapped

---
+++ SlackLikeSlideMenu/Controllers/ListController.swift
@@ -0,0 +1,30 @@
+//
+//  ListController.swift
+//  SlackLikeSlideMenu
+//
+//  Created by Nuno Pereira on 01/11/2018.
+//  Copyright © 2018 Nuno Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
